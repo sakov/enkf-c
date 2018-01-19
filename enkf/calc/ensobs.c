233195eb4f095cad9213d1e541e5064af4411028
Pavel Sakov <sakov@users.noreply.github.com>
2018-01-19 14:17:00 +1100

    cosmetics

--- enkf/calc/ensobs.c
+++ enkf/calc/ensobs.c
@@ -338,8 +338,12 @@ void das_calcinnandspread(dasystem* das)
                 continue;
             das->std_f[o] = sqrt(das->std_f[o] / (double) (das->nmem - 1));
             das->s_f[o] = m->value - das->s_f[o];
-            if (!isfinite(das->s_f[o]) || fabs(das->s_f[o]) > STATE_BIGNUM)
+            if (!isfinite(das->s_f[o]) || fabs(das->s_f[o]) > STATE_BIGNUM) {
+                enkf_flush();
+                enkf_printf("\n  obs # %d: ", ii);
+                obs_printob(allobs, ii);
                 enkf_quit("obs # %d: Hx = %.3g, no point to continue", o, das->s_f[o]);
+            }
         }
 
         das->s_mode = S_MODE_HA_f;
@@ -383,8 +387,12 @@ void das_calcinnandspread(dasystem* das)
                 continue;
             das->std_a[o] = sqrt(das->std_a[o] / (double) (das->nmem - 1));
             das->s_a[o] = m->value - das->s_a[o];
-            if (!isfinite(das->s_a[o]) || fabs(das->s_a[o]) > STATE_BIGNUM)
+            if (!isfinite(das->s_a[o]) || fabs(das->s_a[o]) > STATE_BIGNUM) {
+                enkf_flush();
+                enkf_printf("\n  obs # %d: ", ii);
+                obs_printob(allobs, ii);
                 enkf_quit("obs # %d: Hx = %d, no point to continue", o);
+            }
         }
 
         das->s_mode = S_MODE_HA_a;
