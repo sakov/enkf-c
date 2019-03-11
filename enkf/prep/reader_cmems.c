e8100f7ea26ce02c03855bb90ca0e0e9fdfa5656
Pavel Sakov <sakov@users.noreply.github.com>
2019-03-12 09:06:35 +1100

    v1.92.4

--- enkf/prep/reader_cmems.c
+++ enkf/prep/reader_cmems.c
@@ -316,7 +316,8 @@ void reader_cmems_standard(char* fname, int fid, obsmeta* meta, grid* g, observa
                 continue;
             {
                 qcflagint = (int) qcflag[p][i] - (int) '0';
-                assert(qcflagint >= 0 && qcflagint <= QCFLAGVALMAX);
+                if (qcflagint < 0 || qcflagint > QCFLAGVALMAX) /* impossible */
+		    continue;
                 if (!((1 << qcflagint) & qcflagvals))
                     continue;
             }
