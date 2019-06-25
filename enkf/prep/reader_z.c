3a0938a9c9d30c4a3849f0e353633b6583ef724f
Pavel Sakov <sakov@users.noreply.github.com>
2019-06-25 11:05:54 +1000

    v1.100.0 (added reader_z.c this time)

---
+++ enkf/prep/reader_z.c
@@ -0,0 +1,342 @@
+/******************************************************************************
+ *
+ * File:        reader_z.c        
+ *
+ * Created:     25/06/2019
+ *
+ * Author:      Pavel Sakov
+ *              Bureau of Meteorology
+ *
+ * Description: Generic reader for observations in a single profile.
+ *                It currently assumes the following:
+ *                - longitude and latitude are either 0-dimensional
+ *                  variables or 1-dimensional variables of size 1;
+ *                - time is either 0-dimensional, 1-dimensional of size 1, or
+ *                  1-dimensional of full size (nz).
+ *                - profile variables are either 1-dimensional or 3-4
+ *                  dimensional with dummy dimensions of size 1 (yes, this has
+ *                  little sense, but some providers do use such formats)
+ *                There are a number of parameters that must (++) or can be
+ *              specified if they differ from the default value (+). Some
+ *              parameters are optional (-):
+ *              - VARNAME (++)
+ *              - TIMENAME ("time") (+)
+ *              - LONNAME ("lon" | "longitude") (+)
+ *              - LATNAME ("lat" | "latitude") (+)
+ *              - ZNAME ("z") (+)
+ *              - ESTDNAME ("error_std") (-)
+ *                  error STD; if absent then needs to be specified externally
+ *                  in the observation data parameter file
+ *              - VARSHIFT (-)
+ *                  data offset to be added
+ *              - FOOTRPINT (-)
+ *                  footprint of observations in km
+ *              - MINDEPTH (-)
+ *                  minimal allowed depth
+ *              - MAXDEPTH (-)
+ *                  maximal allowed depth
+ *              - INSTRUMENT (-)
+ *                  instrument string that will be used for calculating
+ *                  instrument stats
+ *              - QCFLAGNAME (-)
+ *                  name of the QC flag variable, 0 <= qcflag <= 31
+ *              - QCFLAGVALS (-)
+ *                  the list of allowed values of QC flag variable
+ *              Note: it is possible to have multiple entries of QCFLAGNAME and
+ *                QCFLAGVALS combination, e.g.:
+ *                  PARAMETER QCFLAGNAME = TEMP_quality_control
+ *                  PARAMETER QCFLAGVALS = 1
+ *                  PARAMETER QCFLAGNAME = DEPTH_quality_control
+ *                  PARAMETER QCFLAGVALS = 1
+ *                  PARAMETER QCFLAGNAME = LONGITUDE_quality_control
+ *                  PARAMETER QCFLAGVALS = 1,8
+ *                  PARAMETER QCFLAGNAME = LATITUDE_quality_control
+ *                  PARAMETER QCFLAGVALS = 1,8
+ *                An observation is considered valid if each of the specified
+ *                flags takes a permitted value.
+ *
+ *****************************************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <assert.h>
+#include "ncw.h"
+#include "definitions.h"
+#include "utils.h"
+#include "obsprm.h"
+#include "model.h"
+#include "grid.h"
+#include "observations.h"
+#include "prep_utils.h"
+#include "allreaders.h"
+
+#define TYPE_DOUBLE 0
+#define TYPE_SHORT 1
+
+/**
+ */
+void reader_z(char* fname, int fid, obsmeta* meta, grid* g, observations* obs)
+{
+    char* varname = NULL;
+    char* lonname = NULL;
+    char* latname = NULL;
+    char* zname = NULL;
+    char* estdname = NULL;
+    char* timename = NULL;
+    int nqcflags = 0;
+    char** qcflagname = NULL;
+    uint32_t* qcflagvals = 0;
+
+    int ncid;
+    double varshift = 0.0;
+    char instrument[MAXSTRLEN];
+
+    size_t nobs = 0;
+    int varid;
+    double lon = NAN;
+    double lat = NAN;
+    double* var = NULL;
+    double* z = NULL;
+    double var_estd = NAN;
+    double* estd = NULL;
+    uint32_t** qcflag = NULL;
+    int have_time = 1;
+    int singletime = -1;
+    double* time = NULL;
+    char tunits[MAXSTRLEN];
+    double tunits_multiple = NAN, tunits_offset = NAN;
+    int i, nobs_read;
+
+    strcpy(instrument, meta->product);
+    for (i = 0; i < meta->npars; ++i) {
+        if (strcasecmp(meta->pars[i].name, "VARNAME") == 0)
+            varname = meta->pars[i].value;
+        else if (strcasecmp(meta->pars[i].name, "TIMENAME") == 0)
+            timename = meta->pars[i].value;
+        else if (strcasecmp(meta->pars[i].name, "LONNAME") == 0)
+            lonname = meta->pars[i].value;
+        else if (strcasecmp(meta->pars[i].name, "LATNAME") == 0)
+            latname = meta->pars[i].value;
+        else if (strcasecmp(meta->pars[i].name, "ZNAME") == 0)
+            zname = meta->pars[i].value;
+        else if (strcasecmp(meta->pars[i].name, "ESTDNAME") == 0)
+            estdname = meta->pars[i].value;
+        else if (strcasecmp(meta->pars[i].name, "VARSHIFT") == 0) {
+            if (!str2double(meta->pars[i].value, &varshift))
+                enkf_quit("%s: can not convert VARSHIFT = \"%s\" to double\n", meta->prmfname, meta->pars[i].value);
+            enkf_printf("        VARSHIFT = %s\n", meta->pars[i].value);
+        }
+        /*
+         * (FOOTPRINT, MINDEPTH and MAXDEPTH are handled in obs_add() )
+         */
+        else if (strcasecmp(meta->pars[i].name, "FOOTPRINT") == 0) {
+            double footprint;
+
+            if (!str2double(meta->pars[i].value, &footprint))
+                enkf_quit("observation prm file: can not convert FOOTPRINT = \"%s\" to double\n", meta->pars[i].value);
+            enkf_printf("        FOOTPRINT = %.0f\n", footprint);
+            continue;
+        } else if (strcasecmp(meta->pars[i].name, "MINDEPTH") == 0) {
+            double mindepth;
+
+            if (!str2double(meta->pars[i].value, &mindepth))
+                enkf_quit("observation prm file: can not convert MINDEPTH = \"%s\" to double\n", meta->pars[i].value);
+            enkf_printf("        MINDEPTH = %.0f\n", mindepth);
+            continue;
+        } else if (strcasecmp(meta->pars[i].name, "MAXDEPTH") == 0) {
+            double maxdepth;
+
+            if (!str2double(meta->pars[i].value, &maxdepth))
+                enkf_quit("observation prm file: can not convert MAXDEPTH = \"%s\" to double\n", meta->pars[i].value);
+            enkf_printf("        MAXDEPTH = %.0f\n", maxdepth);
+            continue;
+        } else if (strcasecmp(meta->pars[i].name, "INSTRUMENT") == 0)
+            strncpy(instrument, meta->pars[i].value, MAXSTRLEN - 1);
+        else if (strcasecmp(meta->pars[i].name, "QCFLAGNAME") == 0 || strcasecmp(meta->pars[i].name, "QCFLAGVALS") == 0)
+            /*
+             * QCFLAGNAME and QCFLAGVALS are dealt with separately
+             */
+            ;
+        else
+            enkf_quit("unknown PARAMETER \"%s\"\n", meta->pars[i].name);
+    }
+    get_qcflags(meta, &nqcflags, &qcflagname, &qcflagvals);
+
+    if (varname == NULL)
+        enkf_quit("reader_z(): %s: VARNAME not specified", fname);
+    else
+        enkf_printf("        VARNAME = %s\n", varname);
+
+    ncw_open(fname, NC_NOWRITE, &ncid);
+    ncw_inq_varid(ncid, varname, &varid);
+    ncw_inq_varsize(ncid, varid, &nobs);
+    /*
+     * main variable
+     */
+    var = malloc(nobs * sizeof(double));
+    read_ncvardouble(ncid, varid, nobs, var);
+
+    /*
+     * lon/lat
+     */
+    lonname = get_lonname(ncid, lonname);
+    if (lonname != NULL) {
+        enkf_printf("        LONNAME = %s\n", lonname);
+        ncw_inq_varid(ncid, lonname, &varid);
+    } else
+        enkf_quit("reader_z(): %s: could not find longitude variable", fname);
+    ncw_check_varsize(ncid, varid, 1);
+    ncw_get_var_double(ncid, varid, &lon);
+
+    latname = get_latname(ncid, latname);
+    if (latname != NULL) {
+        enkf_printf("        LATNAME = %s\n", latname);
+        ncw_inq_varid(ncid, latname, &varid);
+    } else
+        enkf_quit("reader_z(): %s: could not find latitude variable", fname);
+    ncw_check_varsize(ncid, varid, 1);
+    ncw_get_var_double(ncid, varid, &lat);
+
+    /*
+     * z
+     */
+    zname = get_zname(ncid, zname);
+    if (zname != NULL) {
+        enkf_printf("        ZNAME = %s\n", zname);
+        ncw_inq_varid(ncid, zname, &varid);
+    } else
+        enkf_quit("reader_z(): %s: could not find Z variable", fname);
+    ncw_check_varsize(ncid, varid, nobs);
+    z = malloc(nobs * sizeof(double));
+    read_ncvardouble(ncid, varid, nobs, z);
+
+    /*
+     * estd
+     */
+    varid = -1;
+    if (estdname != NULL)
+        ncw_inq_varid(ncid, estdname, &varid);
+    else if (ncw_var_exists(ncid, "error_std"))
+        ncw_inq_varid(ncid, "error_std", &varid);
+    if (varid >= 0) {
+        estd = malloc(nobs * sizeof(double));
+        read_ncvardouble(ncid, varid, nobs, estd);
+    }
+
+    if (estd == NULL) {
+        ncw_inq_varid(ncid, varname, &varid);
+        if (ncw_att_exists(ncid, varid, "error_std")) {
+            ncw_check_attlen(ncid, varid, "error_std", 1);
+            ncw_get_att_double(ncid, varid, "error_std", &var_estd);
+        }
+    }
+
+    /*
+     * qcflag
+     */
+    if (nqcflags > 0) {
+        qcflag = alloc2d(nqcflags, nobs, sizeof(int32_t));
+        for (i = 0; i < nqcflags; ++i) {
+            ncw_inq_varid(ncid, qcflagname[i], &varid);
+            ncw_check_vardims(ncid, varid, 1, &nobs);
+            ncw_get_var_uint(ncid, varid, qcflag[i]);
+        }
+    }
+
+    /*
+     * time
+     */
+    timename = get_timename(ncid, timename);
+    if (timename != NULL) {
+        enkf_printf("        TIMENAME = %s\n", timename);
+        ncw_inq_varid(ncid, timename, &varid);
+    } else {
+        enkf_printf("        reader_z(): %s: no TIME variable\n", fname);
+        have_time = 0;
+    }
+
+    if (have_time) {
+        size_t timelen = 0;
+
+        ncw_inq_varsize(ncid, varid, &timelen);
+        if (timelen == 1) {
+            singletime = 1;
+            time = malloc(sizeof(double));
+        } else {
+            singletime = 0;
+            assert(timelen == nobs);
+            time = malloc(nobs * sizeof(double));
+        }
+
+        read_ncvardouble(ncid, varid, timelen, time);
+        ncw_get_att_text(ncid, varid, "units", tunits);
+        tunits_convert(tunits, &tunits_multiple, &tunits_offset);
+    }
+
+    ncw_close(ncid);
+
+    nobs_read = 0;
+    for (i = 0; i < (int) nobs; ++i) {
+        observation* o;
+        int ii;
+
+        if (isnan(var[i]) || (estd != NULL && isnan(estd[i])) || (have_time && !singletime && isnan(time[i])))
+            continue;
+        for (ii = 0; ii < nqcflags; ++ii)
+            if (!(qcflag[ii][i] | qcflagvals[ii]))
+                continue;
+
+        nobs_read++;
+        obs_checkalloc(obs);
+        o = &obs->data[obs->nobs];
+
+        o->product = st_findindexbystring(obs->products, meta->product);
+        assert(o->product >= 0);
+        o->type = obstype_getid(obs->nobstypes, obs->obstypes, meta->type, 1);
+        o->instrument = st_add_ifabsent(obs->instruments, instrument, -1);
+        o->id = obs->nobs;
+        o->fid = fid;
+        o->batch = 0;
+        o->value = (double) (var[i] + varshift);
+        if (estd == NULL)
+            o->estd = var_estd;
+        else
+            o->estd = estd[i];
+        o->lon = lon;
+        o->lat = lat;
+        o->depth = z[i];
+        o->status = grid_xy2fij(g, o->lon, o->lat, &o->fi, &o->fj);
+        if (!obs->allobs && o->status == STATUS_OUTSIDEGRID)
+            continue;
+        if (o->status == STATUS_OK)
+            o->status = grid_z2fk(g, o->fi, o->fj, o->depth, &o->fk);
+        else
+            o->fk = NAN;
+        o->model_depth = NAN;   /* set in obs_add() */
+        if (have_time) {
+            double t = (singletime) ? time[0] : time[i];
+
+            o->time = t * tunits_multiple + tunits_offset;
+        } else
+            o->time = NAN;
+
+        o->aux = -1;
+
+        obs->nobs++;
+    }
+    enkf_printf("        nobs = %d\n", nobs_read);
+
+    free(var);
+    free(z);
+    if (estd != NULL)
+        free(estd);
+    if (time != NULL)
+        free(time);
+    if (nqcflags > 0) {
+        free(qcflagname);
+        free(qcflagvals);
+        free(qcflag);
+    }
+}
