93205078b87d2c963f6c5ca92e1a6c31367962af
Pavel Sakov <sakov@users.noreply.github.com>
2022-01-31 13:51:57 +1100

    v2.11.4

--- enkf/common/ncutils.c
+++ enkf/common/ncutils.c
@@ -1362,7 +1362,7 @@ void ncu_readvarfloat(int ncid, int varid, size_t n, float v[])
     if (ncw_att_exists(ncid, varid, "add_offset")) {
         float add_offset;
 
-        ncw_check_attlen(ncid, varid, "scale_factor", 1);
+        ncw_check_attlen(ncid, varid, "add_offset", 1);
         ncw_get_att_float(ncid, varid, "add_offset", &add_offset);
         for (i = 0; i < n; ++i)
             v[i] += add_offset;
@@ -1750,7 +1750,7 @@ void ncu_readvardouble(int ncid, int varid, size_t n, double v[])
     if (ncw_att_exists(ncid, varid, "add_offset")) {
         double add_offset;
 
-        ncw_check_attlen(ncid, varid, "scale_factor", 1);
+        ncw_check_attlen(ncid, varid, "add_offset", 1);
         ncw_get_att_double(ncid, varid, "add_offset", &add_offset);
         for (i = 0; i < n; ++i)
             v[i] += add_offset;
