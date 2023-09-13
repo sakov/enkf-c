aff70d37d11f9115cc10fdf315e49223db2372b2
Pavel Sakov <sakov@users.noreply.github.com>
2023-09-13 15:20:50 +1000

    v2.22.8

--- enkf/calc/enkf_calc.c
+++ enkf/calc/enkf_calc.c
@@ -349,7 +349,7 @@ int main(int argc, char* argv[])
     /*
      * calculate stats for observation batches and write to FNAME_BATCHES
      */
-#if defined(MPI)
+#if defined(USE_SHMEM)
     if (singleob == NULL && das->obs->nobs > 0 && sm_comm_rank == 0) {
 #else
     if (singleob == NULL && das->obs->nobs > 0) {
